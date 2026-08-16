def61368f82a9b2813214fd97307027d33fae962
Kirtimaan Vishwakarma <vishwakarma.kirtimaan@gmail.com>
2026-08-16 14:04:32 +0530

    added ApiProperty

--- src/user/dto/deleteUser.dto.ts
+++ src/user/dto/deleteUser.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsMongoId } from "class-validator";
 
 export class UserIdDto {
+    @ApiProperty()
     @IsMongoId()
     userId!: string
 }
